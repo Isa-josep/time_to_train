721b21a77eb8e058834610fb7f6cf21f4c69d2ad
Isa-josep <isaurini1902@gmail.com>
2024-02-15 23:51:45 -0600

    provider sope and router fixesd

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,6 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:time_to_train/presentation/screens/homes_creen.dart';
 
 final appRouter = GoRouter(
+  initialLocation: '/',
   routes: [
     GoRoute(
       path: '/',
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:time_to_train/config/router/app_router.dart';
 import 'package:time_to_train/config/theme/app_theme.dart';
-import 'package:time_to_train/presentation/screens/homes_creen.dart';
-
 void main() {
-  const ProviderScope(
-    child: MainApp(),
+  runApp(
+    const ProviderScope(
+      child: MainApp()
+    )
   );
 }
+
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return  MaterialApp.router(
+      title: "Widgets",
+      routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 1).getTheme(),
-      home: const HomeView(),
     );
   }
 }
