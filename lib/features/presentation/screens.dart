fbf3b7797b07093e73fd605cbb80263747eec4d4
Isa-josep <isaurini1902@gmail.com>
2024-02-28 09:18:49 -0600

    barril

--- lib/features/presentation/screens.dart
+++ lib/features/presentation/screens.dart
@@ -3,4 +3,5 @@ export 'screens/theme_change_screen.dart';
 export 'screens/test_screen.dart';
 export 'screens/graph_screen.dart';
 export 'screens/login_screen.dart';
+export 'screens/register_screen.dart';
 //export 'screens/'
