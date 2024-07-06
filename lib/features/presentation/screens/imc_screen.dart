2c9693027ab6b52b9f58593e35c052574a9fae2b
isa <isaurini1902@gmail.com>
2024-07-06 00:26:25 -0600

    scren imc

--- lib/features/presentation/screens/imc_screen.dart
+++ lib/features/presentation/screens/imc_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets.dart';
 
 class ImcScreen extends StatefulWidget {
   const ImcScreen({super.key});
@@ -8,30 +9,47 @@ class ImcScreen extends StatefulWidget {
 }
 
 class _ImcScreenState extends State<ImcScreen> {
-  double _height = 0.0; // Stores user entered height
-  double _weight = 0.0; // Stores user entered weight
-  double _imc = 0.0; // Stores calculated BMI
-  String _interpretation = ""; // Stores BMI interpretation text
+  double _height = 0.0; 
+  double _weight = 0.0; 
+  double _imc = 0.0; 
+  String _interpretation = ""; 
+  bool _showResult = false; 
 
   final TextEditingController _heightController = TextEditingController();
   final TextEditingController _weightController = TextEditingController();
 
   void _calculateImc() {
+    if (_height <= 0 || _weight <= 0) {
+      return;
+    }
     setState(() {
       _imc = _weight / (_height * _height);
       _interpretation = _interpretImc(_imc);
+      _showResult = true;
     });
   }
 
   String _interpretImc(double imc) {
     if (imc < 18.5) {
-      return "Underweight";
+      return "Bajo de peso";
     } else if (imc < 25.0) {
-      return "Normal weight";
+      return "Peso normal";
     } else if (imc < 30.0) {
-      return "Overweight";
+      return "Sobrepeso";
     } else {
-      return "Obese";
+      return "Obesidad";
+    }
+  }
+
+  Color getColorForImc(double imc) {
+    if (imc < 18.5) {
+      return Colors.blue;
+    } else if (imc < 25.0) {
+      return Colors.green;
+    } else if (imc < 30.0) {
+      return Colors.yellow;
+    } else {
+      return Colors.red;
     }
   }
 
@@ -39,46 +57,80 @@ class _ImcScreenState extends State<ImcScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('IMC Calculator'),
+        title: const Text('Calculadora de IMC'),
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            TextField(
+            CustomTextFormField(
+              label: 'Ingresa tu altura (Mts)',
               controller: _heightController,
               keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: "Height (in meters)",
-              ),
               onChanged: (value) {
                 _height = double.tryParse(value) ?? 0.0;
               },
             ),
             const SizedBox(height: 20.0),
-            TextField(
+            CustomTextFormField(
               controller: _weightController,
               keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: "Weight (in kilograms)",
-              ),
+              label: 'Ingresa tu peso (Kg)',
               onChanged: (value) {
                 _weight = double.tryParse(value) ?? 0.0;
               },
             ),
             const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: _calculateImc,
-              child: const Text('Calculate BMI'),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: CustomFilledButton(
+                onPressed: _calculateImc,
+                text: 'Calcular',
+              ),
             ),
             const SizedBox(height: 20.0),
-            Text(
-              'Your BMI: $_imc ($_interpretation)',
-              style: const TextStyle(fontSize: 18.0),
-            ),
+            if (_showResult)
+              Column(
+                children: [
+                  Text(
+                    'Tu indice es : ${_imc.toStringAsFixed(2)} ($_interpretation)',
+                    style: const TextStyle(fontSize: 18.0),
+                  ),
+                  const SizedBox(height: 20.0),
+                  _buildImcThermometer(),
+                ],
+              ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildImcThermometer() {
+    return Column(
+      children: [
+        _buildThermometerSegment('Obesidad', Colors.red, 30, 40),
+        _buildThermometerSegment('Sobrepeso', Colors.yellow, 25, 30),
+        _buildThermometerSegment('Peso normal', Colors.green, 18.5, 25),
+        _buildThermometerSegment('Bajo de peso', Colors.blue, 0, 18.5),
+      ],
+    );
+  }
+
+  Widget _buildThermometerSegment(String label, Color color, double minValue, double maxValue) {
+    final isActive = _imc >= minValue && _imc < maxValue;
+
+    return Row(
+      children: [
+        Container(
+          height: 50,
+          width: 20,
+          color: isActive ? color : color.withOpacity(0.3),
+        ),
+        const SizedBox(width: 10),
+        Text(label, style: TextStyle(color: isActive ? color : color.withOpacity(0.3))),
+      ],
     );
   }
 }
