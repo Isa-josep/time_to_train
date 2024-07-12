6461392ab7d1ea674e822c435366068aef4d6623
isa <isaurini1902@gmail.com>
2024-07-11 20:02:20 -0600

    card rutina add

--- lib/features/presentation/screens/homes_screen.dart
+++ lib/features/presentation/screens/homes_screen.dart
@@ -11,7 +11,7 @@ class HomeView extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final scaffoldKey = GlobalKey<ScaffoldState>();
     final authState = ref.watch(authProvider);
-
+    
     return Scaffold(
       key: scaffoldKey,
       appBar: AppBar(
@@ -51,7 +51,7 @@ class _Home extends StatelessWidget {
                 showModalBottomSheet(
                   context: context,
                   isScrollControlled: true,
-                  builder: (context) => const SectionModal(),
+                  builder: (context) => const RoutineModal(),
                 );
               },
               child: const Icon(Icons.add),
@@ -62,16 +62,21 @@ class _Home extends StatelessWidget {
   }
 }
 
-class SectionModal extends ConsumerStatefulWidget {
-  const SectionModal({super.key});
+class RoutineModal extends ConsumerStatefulWidget {
+  const RoutineModal({super.key});
 
   @override
-  _SectionModalState createState() => _SectionModalState();
+  _RoutineModalState createState() => _RoutineModalState();
 }
 
-class _SectionModalState extends ConsumerState<SectionModal> {
+class _RoutineModalState extends ConsumerState<RoutineModal> {
   final List<Map<String, String>> sections = [];
   String? selectedGroup;
+  final titleController = TextEditingController();
+  final descriptionController = TextEditingController();
+  final List<TextEditingController> videoControllers = [];
+
+  bool addVideoField = false;
 
   void addSection() {
     setState(() {
@@ -85,87 +90,143 @@ class _SectionModalState extends ConsumerState<SectionModal> {
     });
   }
 
+  void addVideoUrlField() {
+    setState(() {
+      videoControllers.add(TextEditingController());
+    });
+  }
+
+  void removeVideoUrlField(int index) {
+    setState(() {
+      videoControllers.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final groups = ref.watch(groupProvider);
+    final scaffoldBackgroundColor = Theme.of(context).scaffoldBackgroundColor;
     return Padding(
       padding: EdgeInsets.only(
         bottom: MediaQuery.of(context).viewInsets.bottom,
       ),
       child: DraggableScrollableSheet(
-        initialChildSize: 0.8,
+        initialChildSize: 0.9, // Ajustado para abrirse más arriba
         maxChildSize: 0.95,
         minChildSize: 0.6,
         builder: (_, controller) => SingleChildScrollView(
           controller: controller,
           child: Padding(
             padding: const EdgeInsets.all(20.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Text('Agregar Secciones', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                const SizedBox(height: 20),
-                DropdownButtonFormField<String>(
-                  decoration: const InputDecoration(labelText: 'Seleccionar Grupo'),
-                  value: selectedGroup,
-                  items: groups.groups.map<DropdownMenuItem<String>>((group) {
-                    return DropdownMenuItem<String>(
-                      value: group['id'].toString(),
-                      child: Text(group['nombre'], style: const TextStyle(fontSize: 16)),
-                    );
-                  }).toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      selectedGroup = value;
-                    });
-                  },
-                ),
-                const SizedBox(height: 20),
-                ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: sections.length,
-                  itemBuilder: (context, index) {
-                    return Column(
+            child: Card(
+              color: scaffoldBackgroundColor, // Fondo blanco puro
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text('Agregar Rutina', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 20),
+                    DropdownButtonFormField<String>(
+                      decoration: const InputDecoration(
+                        labelText: 'Seleccionar Grupo',
+                        border: OutlineInputBorder(),
+                      ),
+                      value: selectedGroup,
+                      items: groups.groups.map<DropdownMenuItem<String>>((group) {
+                        return DropdownMenuItem<String>(
+                          value: group['id'].toString(),
+                          child: Text(group['nombre'], style: const TextStyle(fontSize: 16)),
+                        );
+                      }).toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          selectedGroup = value;
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    CustomTextFormField(
+                      controller: titleController,
+                      label: 'Título',
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0x0fffffff),
+                        borderRadius: BorderRadius.circular(8.0),
+                        border: Border.all(color: Colors.grey),
+                      ),
+                      child: TextFormField(
+                        controller: descriptionController,
+                        maxLines: 5,
+                        decoration: const InputDecoration(
+                          labelText: 'Descripción',
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.all(10.0),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    SwitchListTile(
+                      title: const Text('Agregar enlaces de YouTube'),
+                      value: addVideoField,
+                      onChanged: (bool value) {
+                        setState(() {
+                          addVideoField = value;
+                          if (!value) videoControllers.clear();
+                        });
+                      },
+                    ),
+                    if (addVideoField)
+                      Column(
+                        children: [
+                          ...videoControllers.map((controller) {
+                            final index = videoControllers.indexOf(controller);
+                            return Column(
+                              children: [
+                        
+                                   CustomTextFormField(
+                                    controller: controller,
+                                    label: 'Enlace de YouTube',
+                                  ),
+                                
+                                IconButton(
+                                  icon: const Icon(Icons.remove_circle, color: Colors.red),
+                                  onPressed: () => removeVideoUrlField(index),
+                                ),
+                                const SizedBox(height: 10),
+                              ],
+                            );
+                          }).toList(),
+                          CustomFilledButton(
+                            onPressed: addVideoUrlField,
+                            text: 'Agregar Enlace',
+                          ),
+                        ],
+                      ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        CustomTextFormField(
-                          label: 'Título de Sección',
-                          onChanged: (value) {
-                            sections[index]['title'] = value;
+                        CustomFilledButton(
+                          onPressed: () {
+                            Navigator.pop(context);
                           },
+                          text: 'Cancelar',
                         ),
-                        const SizedBox(height: 20),
-                        CustomTextFormField(
-                          label: 'Contenido de Sección',
-                          onChanged: (value) {
-                            sections[index]['content'] = value;
-                          },
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.remove_circle, color: Colors.red),
-                          onPressed: () => removeSection(index),
+                        CustomFilledButton(
+                          onPressed: addSection,
+                          text: 'Agregar Sección',
                         ),
                       ],
-                    );
-                  },
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    CustomFilledButton(
-                      onPressed: addSection,
-                      text: 'Agregar Sección',
-                    ),
-                    CustomFilledButton(
-                      onPressed: () {
-                        // Lógica para guardar las secciones
-                        Navigator.pop(context);
-                      },
-                      text: 'Guardar',
                     ),
                   ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
